31e60990f093e0e1d6eb54b52d334163b38fa6d6
Roman Gardukevich <roman@gardukevich.com>
2015-04-05 10:54:44 +0300

    Initial Commit

---
+++ ByApartments/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ByApartments
+//
+//  Created by odnairy on 05/04/15.
+//  Copyright (c) 2015 Roman Gardukevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ByApartments/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ByApartments
+//
+//  Created by odnairy on 05/04/15.
+//  Copyright (c) 2015 Roman Gardukevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
