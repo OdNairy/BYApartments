e7a7f51d14cc4b8d57f1cd0ab306a718740cbbc6
Roman Gardukevich <roman@gardukevich.com>
2015-08-04 16:42:25 +0300

    Blank project on Swift and Cocoapods setup

---
+++ ByApartments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ByApartments
+//
+//  Created by odnairy on 04/08/15.
+//  Copyright © 2015 Romanus LC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
